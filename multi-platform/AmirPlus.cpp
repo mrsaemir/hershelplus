3351d3897e534051df4c799faf3c9b02474f3a54
Amir <saemiamirhossein@gmail.com>
2025-01-05 22:37:20 +0100

    Add functionality to collect fingerprints

---
+++ multi-platform/AmirPlus.cpp
@@ -0,0 +1,29 @@
+#include "LiveFingerprinter.h"
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    // Check for correct number of arguments
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << "<IFNAME> <IP_ADDRESS> <PORT>" << std::endl;
+        return -1;
+    }
+
+    // Parse IP address and port from arguments
+    char* ifname = argv[1];
+    char* ip = argv[2];
+    unsigned short port = static_cast<unsigned short>(std::stoi(argv[2]));
+    printf("Getting Signature for %s:%d\n", ip, port);
+
+    LiveFingerprinter lf;
+    if (lf.setupPcapAdapter(ifname) < 0){
+        printf("Error setting up pcap!\n");
+        exit(-1);
+    }
+
+    Signature s;
+    if (lf.getFingerprint(ip, port, s) < 0){
+        printf("Error getting fingerprint\n");
+        exit(-1);
+    }
+}
